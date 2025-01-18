0d2004f46c32ee3ff440417b6a95672713fa6919
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2025-01-18 18:46:29 -0500

    Add classes to application

--- main.py
+++ main.py
@@ -4,123 +4,151 @@ from tkinter import ttk, CENTER, W, NO, YES
 from backend import insert, read, delete, update
 
 
-def insert_film():
-    '''Insert record into database'''
-    
-    insert(title_entry.get(), length_entry.get(), year_entry.get())
-    title_entry.delete(0, tk.END)
-    length_entry.delete(0, tk.END)
-    year_entry.delete(0, tk.END)
-    view_all()
+def main():
+    window = Application()
+    window.mainloop()
 
 
-def select_film(e):
-    '''Select a film'''
+class Application(tk.Tk):
 
-    title_entry.delete(0, tk.END)
-    length_entry.delete(0, tk.END)
-    year_entry.delete(0, tk.END)
-
-    selected = tree.selection()
-    if selected:
-        item = tree.item(selected)
-        values = item['values']
-
-        title_entry.insert(0, values[1])
-        length_entry.insert(0, values[2])
-        year_entry.insert(0, values[3])
-
-
-def update_film():
-    '''Update a record in database'''
-
-    selected = tree.selection()
-    if selected:
-        item = tree.item(selected)
-        id = item['values'][0]
-        update(id, title_entry.get(), length_entry.get(), year_entry.get())
-
-
-def delete_film():
-    '''Delete films from database'''
-
-    for i in tree.selection():
-        t = tree.item(i)
-        id = t['values'][0]
-        delete(id)
-    view_all()
-
-
-def view_all():
-    '''View all records'''
-
-    tree.delete(*tree.get_children())
-    rows = read()
-    for idx, record in enumerate(rows):
-        tree.insert(parent='', index='end', text="", values=(record[0], record[1], record[2], record[3]))
+    def __init__(self):
+        super().__init__()
         
+        self.title("Film Database")
+        self.geometry("900x500")
+        self.columnconfigure(0, weight=0)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(8, weight=1)
 
-window = tk.Tk()
-window.title("Film Database")
-window.geometry("900x500")
-window.columnconfigure(0, weight=0)
-window.columnconfigure(1, weight=1)
-window.rowconfigure(8, weight=1)
+        self.window_label = tk.Label(self, text="Film Database", font=('Arial 12 bold'))
+        self.window_label.grid(row=4, column=0, columnspan=4, pady=15, padx=15)
 
-window_label = tk.Label(window, text="Film Database", font=('Arial 12 bold'))
-window_label.grid(row=0, column=0, pady=15, padx=15) 
+        # Entry fields and labels
+        self.title_label = tk.Label(self, text="Title:")
+        self.title_label.grid(row=1, column=0, sticky="w", padx=5, pady=5)
 
-# Entry fields and labels
-title_label = tk.Label(window, text="Title:")
-title_label.grid(row=1, column=0, sticky="w", padx=5, pady=5) 
-title_entry = tk.Entry(window)
-title_entry.grid(row=1, column=1, columnspan=3, sticky="ew", padx=5, pady=5) 
+        self.title_entry = tk.Entry(self)
+        self.title_entry.grid(row=1, column=1, columnspan=3, sticky="ew", padx=5, pady=5)
 
-length_label = tk.Label(window, text="Length:")
-length_label.grid(row=2, column=0, sticky="w", padx=5, pady=5) 
-length_entry = tk.Entry(window)
-length_entry.grid(row=2, column=1, columnspan=3, sticky="ew", padx=5, pady=5) 
+        self.length_label = tk.Label(self, text="Length:")
+        self.length_label.grid(row=2, column=0, sticky="w", padx=5, pady=5)
 
-year_label = tk.Label(window, text="Year:")
-year_label.grid(row=3, column=0, sticky="w", padx=5, pady=5) 
-year_entry = tk.Entry(window)
-year_entry.grid(row=3, column=1, columnspan=3, sticky="ew", padx=5, pady=5) 
+        self.length_entry = tk.Entry(self)
+        self.length_entry.grid(row=2, column=1, columnspan=3, sticky="ew", padx=5, pady=5)
 
-# Buttons
-button_frame = tk.Frame(window)
-button_frame.grid(row=4, column=0, columnspan=4, sticky="w", padx=5, pady=5)
+        self.year_label = tk.Label(self, text="Year:")
+        self.year_label.grid(row=3, column=0, sticky="w", padx=5, pady=5)
 
-insert_button = tk.Button(button_frame, text="Insert", width=10, bg="#0099ff", command=insert_film)
-insert_button.pack(side=tk.LEFT, padx=2)
+        self.year_entry = tk.Entry(self)
+        self.year_entry.grid(row=3, column=1, columnspan=3, sticky="ew", padx=5, pady=5)
 
-view_button = tk.Button(button_frame, text="View", width=10, bg="#ffff00", command=view_all)
-view_button.pack(side=tk.LEFT, padx=2)
-
-delete_button = tk.Button(button_frame, text="Delete", width=10, bg="#e62e00", command=delete_film)
-delete_button.pack(side=tk.LEFT, padx=2)
-
-update_button = tk.Button(button_frame, text="Update", width=10, bg="#00ff00", command=update_film)
-update_button.pack(side=tk.LEFT, padx=2)
-
-# Treeview
-tree = ttk.Treeview(window)
-tree['columns'] = ('id', 'title', 'length', 'year')
-
-tree.column('#0', width=0, stretch=NO)
-tree.column('id', anchor=CENTER, width=10)
-tree.column('title', anchor=W, width=300, minwidth=200, stretch=YES)
-tree.column('length', anchor=CENTER, width=50)
-tree.column('year', anchor=CENTER, width=50)
-
-tree.heading('#0', text='', anchor=W)
-tree.heading('id', text='id')
-tree.heading('title', text='title')
-tree.heading('length', text='length')
-tree.heading('year', text='year')
-
-tree.bind("<<TreeviewSelect>>", select_film)
-tree.grid(row=8, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+        self.frame = Form(self)
+        self.frame.grid(row=4, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
 
 
-window.mainloop()
+class Form(tk.Frame):
 
+    def __init__(self, parent):
+        super().__init__(parent)
+        for i in range(4):
+            self.columnconfigure(i, weight=1)
+
+        self.rowconfigure(0, weight=1)
+        self.parent = parent
+
+        self.insert = insert
+        self.read = read
+        self.delete = delete
+        self.update = update
+
+        # Buttons
+        self.insert_button = tk.Button(self, text="Insert", width=10, bg="#0099ff", command=self.insert_film)
+        self.insert_button.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
+
+        self.view_button = tk.Button(self, text="View", width=10, bg="#ffff00", command=self.view_all)
+        self.view_button.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
+
+        self.delete_button = tk.Button(self, text="Delete", width=10, bg="#e62e00", command=self.delete_film)
+        self.delete_button.grid(row=5, column=2, sticky="ew", padx=5, pady=5)
+
+        self.update_button = tk.Button(self, text="Update", width=10, bg="#00ff00", command=self.update_film)
+        self.update_button.grid(row=5, column=3, sticky="ew", padx=5, pady=5) 
+
+        self.tree_frame = tk.Frame(self)
+        self.tree_frame.grid(row=8, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+        self.tree_frame.columnconfigure(0, weight=1)
+        self.tree_frame.rowconfigure(0, weight=1)
+
+        self.tree = ttk.Treeview(self.tree_frame)
+        self.tree['columns'] = ('id', 'title', 'length', 'year')
+
+        self.tree.column('#0', width=0, stretch=NO)
+        self.tree.column('id', anchor=CENTER, width=10)
+        self.tree.column('title', anchor=W, width=300, minwidth=200, stretch=YES)
+        self.tree.column('length', anchor=CENTER, width=50)
+        self.tree.column('year', anchor=CENTER, width=50)
+
+        self.tree.heading('#0', text='', anchor=W)
+        self.tree.heading('id', text='id')
+        self.tree.heading('title', text='title')
+        self.tree.heading('length', text='length')
+        self.tree.heading('year', text='year')
+
+        self.tree.bind("<<TreeviewSelect>>", self.select_film)
+        self.tree.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+        
+    def insert_film(self):
+        '''Insert record into database'''
+        
+        self.insert(self.parent.title_entry.get(), self.parent.length_entry.get(), self.parent.year_entry.get())
+        self.parent.title_entry.delete(0, tk.END)
+        self.parent.length_entry.delete(0, tk.END)
+        self.parent.year_entry.delete(0, tk.END)
+        self.view_all()
+
+    def select_film(self, e):
+        '''Select a film'''
+
+        self.parent.title_entry.delete(0, tk.END)
+        self.parent.length_entry.delete(0, tk.END)
+        self.parent.year_entry.delete(0, tk.END)
+
+        selected = self.tree.selection()
+        if selected:
+            item = self.tree.item(selected)
+            values = item['values']
+
+            self.parent.title_entry.insert(0, values[1])
+            self.parent.length_entry.insert(0, values[2])
+            self.parent.year_entry.insert(0, values[3])
+
+    def update_film(self):
+        '''Update a record in database'''
+
+        selected = self.tree.selection()
+        if selected:
+            item = self.tree.item(selected)
+            id = item['values'][0]
+            self.update(id, self.parent.title_entry.get(), self.parent.length_entry.get(), self.parent.year_entry.get())
+        self.view_all()
+
+    def delete_film(self):
+        '''Delet.parente films from database'''
+
+        for i in self.tree.selection():
+            t = self.tree.item(i)
+            id = t['values'][0]
+            self.delete(id)
+        self.view_all()
+
+    def view_all(self):
+        '''View all records'''
+
+        self.tree.delete(*self.tree.get_children())
+        rows = self.read()
+        for idx, record in enumerate(rows):
+            self.tree.insert(parent='', index='end', text="", values=(record[0], record[1], record[2], record[3]))
+
+
+if __name__ == "__main__":
+    main()
