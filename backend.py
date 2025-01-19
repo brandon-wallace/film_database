d5c28952996f5f8bd44a9cba3be650d5365cc0f3
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2025-01-18 19:14:40 -0500

    Remove hardcoded db name

--- backend.py
+++ backend.py
@@ -2,10 +2,12 @@
 import sqlite3
 
 
+DATABASE = 'database.db'
+
 def create_table():
     ''' Create table if not existing '''
 
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS films (
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +21,7 @@ def create_table():
 def insert(title, length, year):
     '''Add entry into database.'''
 
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO films (title, length, year) VALUES (?, ?, ?)", (title, length, year))
     conn.commit()
@@ -29,7 +31,7 @@ def insert(title, length, year):
 def read():
     '''Query all records.'''
 
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM films")
     rows = cursor.fetchall()
@@ -40,7 +42,7 @@ def read():
 def delete(id):
     '''Delete a record from database.'''
 
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
     cur.execute("DELETE FROM films WHERE id=?", (id,))
     conn.commit()
@@ -50,7 +52,7 @@ def delete(id):
 def update(id, title, length, year):
     '''Update a database records.'''
 
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
     cur.execute("UPDATE films SET title=?, length=?, year=? WHERE id=?", (title, length, year, id))
     conn.commit()
